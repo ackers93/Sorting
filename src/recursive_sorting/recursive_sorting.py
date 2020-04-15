63b4fbcd5806bf840f7ee885f6582f2cf2386fef
Andrew Ackerman <andrew.ackers@gmail.com>
2020-04-15 12:46:46 -0700

    day 2 MVP

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,15 +1,41 @@
-# TO-DO: complete the helpe function below to merge 2 sorted arrays
-def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
+# TO-DO: complete the helper function below to merge 2 sorted arrays
+def merge(arrA, arrB):
+    elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
     # TO-DO
-    
+    a = 0
+    b = 0
+    for i in range(0, elements):  # loop over merged array
+        # if 'a' variable is greater than or equal to length of 'left' array (arrA)
+        # then assign index position 'b' of 'arrB' to merged_arr at index position 'i'
+        # then increment b by 1.
+        if a >= len(arrA):
+            merged_arr[i] = arrB[b]
+            b += 1
+        elif b >= len(arrB):
+            merged_arr[i] = arrA[a]
+            a += 1
+        elif arrA[a] < arrB[b]:
+            merged_arr[i] = arrA[a]
+            a += 1
+        else:
+            merged_arr[i] = arrB[b]
+            b += 1
+
     return merged_arr
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
-def merge_sort( arr ):
+def merge_sort(arr):
     # TO-DO
+    # if the 'arr' length is more than 1
+    if len(arr) > 1:
+        # store the first/left half of the array in a variable
+        left = merge_sort(arr[0:int(len(arr) / 2)])
+        # store the final/right half of the array in a variable
+        right = merge_sort(arr[int(len(arr) / 2):])
+        # run the array through the 'merge' method
+        arr = merge(left, right)
 
     return arr
 
@@ -20,7 +46,8 @@ def merge_in_place(arr, start, mid, end):
 
     return arr
 
-def merge_sort_in_place(arr, l, r): 
+
+def merge_sort_in_place(arr, l, r):
     # TO-DO
 
     return arr
@@ -28,6 +55,6 @@ def merge_sort_in_place(arr, l, r):
 
 # STRETCH: implement the Timsort function below
 # hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
-def timsort( arr ):
+def timsort(arr):
 
     return arr
